29dda433212eedbaccbbf4ca1b225572938d28e8
PhuTam54 <phutamytb@gmail.com>
2023-06-08 17:14:55 +0700

    Add Main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    public static Stage mainStage;xxx
+    public static Stage mainStage;
 
     public static void main(String[] args) {
         launch(args);
